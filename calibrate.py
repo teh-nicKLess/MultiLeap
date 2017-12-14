0e2eb7d898f2fa234f6ff0da381bdff47be67740
Simon Kappe <simon.kappe@kit.edu>
2017-12-14 14:54:39 +0100

    Simple calibration detection

--- calibrate.py
+++ calibrate.py
@@ -3,192 +3,12 @@
 import sys
 import json
 import numpy as np
-import math
-
 
 if sys.version_info[0] > 2:
     raise Exception("Only python 2 currently")
 
 import websocket
 
-# Transformaiton computation is from here: https://www.lfd.uci.edu/~gohlke/code/transformations.py.html
-
-# epsilon for testing whether a number is close to zero
-_EPS = np.finfo(float).eps * 4.0
-
-def vector_norm(data, axis=None, out=None):
-    """Return length, i.e. Euclidean norm, of ndarray along axis.
-
-    >>> v = np.random.random(3)
-    >>> n = vector_norm(v)
-    >>> np.allclose(n, np.linalg.norm(v))
-    True
-    >>> v = np.random.rand(6, 5, 3)
-    >>> n = vector_norm(v, axis=-1)
-    >>> np.allclose(n, np.sqrt(np.sum(v*v, axis=2)))
-    True
-    >>> n = vector_norm(v, axis=1)
-    >>> np.allclose(n, np.sqrt(np.sum(v*v, axis=1)))
-    True
-    >>> v = np.random.rand(5, 4, 3)
-    >>> n = np.empty((5, 3))
-    >>> vector_norm(v, axis=1, out=n)
-    >>> np.allclose(n, np.sqrt(np.sum(v*v, axis=1)))
-    True
-    >>> vector_norm([])
-    0.0
-    >>> vector_norm([1])
-    1.0
-
-    """
-    data = np.array(data, dtype=np.float64, copy=True)
-    if out is None:
-        if data.ndim == 1:
-            return math.sqrt(np.dot(data, data))
-        data *= data
-        out = np.atleast_1d(np.sum(data, axis=axis))
-        np.sqrt(out, out)
-        return out
-    else:
-        data *= data
-        np.sum(data, axis=axis, out=out)
-        np.sqrt(out, out)
-
-def quaternion_matrix(quaternion):
-    """Return homogeneous rotation matrix from quaternion.
-
-    >>> M = quaternion_matrix([0.99810947, 0.06146124, 0, 0])
-    >>> np.allclose(M, rotation_matrix(0.123, [1, 0, 0]))
-    True
-    >>> M = quaternion_matrix([1, 0, 0, 0])
-    >>> np.allclose(M, np.identity(4))
-    True
-    >>> M = quaternion_matrix([0, 1, 0, 0])
-    >>> np.allclose(M, np.diag([1, -1, -1, 1]))
-    True
-
-    """
-    q = np.array(quaternion, dtype=np.float64, copy=True)
-    n = np.dot(q, q)
-    if n < _EPS:
-        return np.identity(4)
-    q *= math.sqrt(2.0 / n)
-    q = np.outer(q, q)
-    return np.array([
-        [1.0-q[2, 2]-q[3, 3],     q[1, 2]-q[3, 0],     q[1, 3]+q[2, 0], 0.0],
-        [    q[1, 2]+q[3, 0], 1.0-q[1, 1]-q[3, 3],     q[2, 3]-q[1, 0], 0.0],
-        [    q[1, 3]-q[2, 0],     q[2, 3]+q[1, 0], 1.0-q[1, 1]-q[2, 2], 0.0],
-        [                0.0,                 0.0,                 0.0, 1.0]])
-
-
-# https://www.lfd.uci.edu/~gohlke/code/transformations.py.html#line-882
-def affine_matrix_from_points(v0, v1, shear=True, scale=True, usesvd=True):
-    """Return affine transform matrix to register two point sets.
-
-    v0 and v1 are shape (ndims, \*) arrays of at least ndims non-homogeneous
-    coordinates, where ndims is the dimensionality of the coordinate space.
-
-    If shear is False, a similarity transformation matrix is returned.
-    If also scale is False, a rigid/Euclidean transformation matrix
-    is returned.
-
-    By default the algorithm by Hartley and Zissermann [15] is used.
-    If usesvd is True, similarity and Euclidean transformation matrices
-    are calculated by minimizing the weighted sum of squared deviations
-    (RMSD) according to the algorithm by Kabsch [8].
-    Otherwise, and if ndims is 3, the quaternion based algorithm by Horn [9]
-    is used, which is slower when using this Python implementation.
-
-    The returned matrix performs rotation, translation and uniform scaling
-    (if specified).
-
-    >>> v0 = [[0, 1031, 1031, 0], [0, 0, 1600, 1600]]
-    >>> v1 = [[675, 826, 826, 677], [55, 52, 281, 277]]
-    >>> affine_matrix_from_points(v0, v1)
-    array([[   0.14549,    0.00062,  675.50008],
-           [   0.00048,    0.14094,   53.24971],
-           [   0.     ,    0.     ,    1.     ]])
-    >>> T = translation_matrix(np.random.random(3)-0.5)
-    >>> R = random_rotation_matrix(np.random.random(3))
-    >>> S = scale_matrix(random.random())
-    >>> M = concatenate_matrices(T, R, S)
-    >>> v0 = (np.random.rand(4, 100) - 0.5) * 20
-    >>> v0[3] = 1
-    >>> v1 = np.dot(M, v0)
-    >>> v0[:3] += np.random.normal(0, 1e-8, 300).reshape(3, -1)
-    >>> M = affine_matrix_from_points(v0[:3], v1[:3])
-    >>> np.allclose(v1, np.dot(M, v0))
-    True
-
-    More examples in superimposition_matrix()
-
-    """
-    v0 = np.array(v0, dtype=np.float64, copy=True)
-    v1 = np.array(v1, dtype=np.float64, copy=True)
-
-    ndims = v0.shape[0]
-    if ndims < 2 or v0.shape[1] < ndims or v0.shape != v1.shape:
-        raise ValueError("input arrays are of wrong shape or type")
-
-    # move centroids to origin
-    t0 = -np.mean(v0, axis=1)
-    M0 = np.identity(ndims+1)
-    M0[:ndims, ndims] = t0
-    v0 += t0.reshape(ndims, 1)
-    t1 = -np.mean(v1, axis=1)
-    M1 = np.identity(ndims+1)
-    M1[:ndims, ndims] = t1
-    v1 += t1.reshape(ndims, 1)
-
-    if shear:
-        # Affine transformation
-        A = np.concatenate((v0, v1), axis=0)
-        u, s, vh = np.linalg.svd(A.T)
-        vh = vh[:ndims].T
-        B = vh[:ndims]
-        C = vh[ndims:2*ndims]
-        t = np.dot(C, np.linalg.pinv(B))
-        t = np.concatenate((t, np.zeros((ndims, 1))), axis=1)
-        M = np.vstack((t, ((0.0,)*ndims) + (1.0,)))
-    elif usesvd or ndims != 3:
-        # Rigid transformation via SVD of covariance matrix
-        u, s, vh = np.linalg.svd(np.dot(v1, v0.T))
-        # rotation matrix from SVD orthonormal bases
-        R = np.dot(u, vh)
-        if np.linalg.det(R) < 0.0:
-            # R does not constitute right handed system
-            R -= np.outer(u[:, ndims-1], vh[ndims-1, :]*2.0)
-            s[-1] *= -1.0
-        # homogeneous transformation matrix
-        M = np.identity(ndims+1)
-        M[:ndims, :ndims] = R
-    else:
-        # Rigid transformation matrix via quaternion
-        # compute symmetric matrix N
-        xx, yy, zz = np.sum(v0 * v1, axis=1)
-        xy, yz, zx = np.sum(v0 * np.roll(v1, -1, axis=0), axis=1)
-        xz, yx, zy = np.sum(v0 * np.roll(v1, -2, axis=0), axis=1)
-        N = [[xx+yy+zz, 0.0,      0.0,      0.0],
-             [yz-zy,    xx-yy-zz, 0.0,      0.0],
-             [zx-xz,    xy+yx,    yy-xx-zz, 0.0],
-             [xy-yx,    zx+xz,    yz+zy,    zz-xx-yy]]
-        # quaternion: eigenvector corresponding to most positive eigenvalue
-        w, V = np.linalg.eigh(N)
-        q = V[:, np.argmax(w)]
-        q /= vector_norm(q)  # unit quaternion
-        # homogeneous transformation matrix
-        M = quaternion_matrix(q)
-
-    if scale and not shear:
-        # Affine transformation; scale is ratio of RMS deviations from centroid
-        v0 *= v0
-        v1 *= v1
-        M[:ndims, :ndims] *= math.sqrt(np.sum(v1) / np.sum(v0))
-
-    # move centroids back
-    M = np.dot(np.linalg.inv(M1), np.dot(M, M0))
-    M /= M[ndims, ndims]
-    return M
 
 def on_message(ws, message):
     data = json.loads(message)
@@ -199,29 +19,26 @@ def on_message(ws, message):
     if len(tools) != 2:
         return
 
-    origin = ((tools[0][0] - 150 * tools[0][1]) + (tools[1][0] - 150 * tools[1][1])) / 2
-    current = 0.001 * np.array([origin, tools[0][0], tools[1][0]])
-    reference = np.array(pattern)
+    pos0 = 0.001 * np.array(tools[0][0])
+    pos1 = 0.001 * np.array(tools[1][0])
+    dir0 = np.array(tools[0][1])
+    dir1 = np.array(tools[1][1])
 
-    M = affine_matrix_from_points(current, reference, False, False)
+    position = ((pos0 - 0.15 * dir0) + (pos1 - 0.15 * dir1)) / 2
+    direction = pos1 - pos0
+    direction = direction / np.linalg.norm(direction)
+    tmp = position-pos0
+    normal = np.cross(tmp, direction)
+    normal = normal / np.linalg.norm(normal)
 
-    print "Transformation:"
-    print M
+    print "position:", position
+    print "direction:", direction
+    print "normal:", normal
 
 
 def on_open(ws):
     print('OPEN')
     ws.send('{\"background\": true}')
-
-
-table_width = 0.8046
-table_length = 1.431
-
-pos_a = np.array([-table_width / 2, -table_length / 2, 0])
-pos_b = np.array([-table_width / 2, table_length / 2, 0])
-pos_c = np.array([table_width / 2, -table_length / 2, 0])
-pos_d = np.array([table_width / 2, table_length / 2, 0])
-pos = np.array([0, 0, 0])
 
 host = 'localhost'
 port = '6437'
@@ -231,22 +48,6 @@ if len(sys.argv) > 1:
 
 if len(sys.argv) > 2:
     port = sys.argv[2]
-
-if len(sys.argv) > 3:
-    field = sys.argv[3]
-    if field == "A":
-        pos = pos_a
-    elif field == "B":
-        pos = pos_b
-    elif field == "C":
-        pos = pos_c
-    elif field == "D":
-        pos = pos_d
-    else:
-        print "Error: Invalid field"
-        sys.exit(1)
-
-pattern = [pos, pos + np.array([0, -0.057403, 0.138582]), pos + np.array([0, 0.057403, 0.138582])]
 
 url = 'ws://' + host + ':' + port + '/v6.json'
 
